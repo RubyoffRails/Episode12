998f24bb7321708fb04dd9c1b47de7dda2e6e317
Melissa Holmes <meltar@gmail.com>
2013-06-08 01:44:54 -0400

    Panda complete

--- minitest/navy_test.rb
+++ minitest/navy_test.rb
@@ -9,6 +9,10 @@ class TestAdmiral < MiniTest::Unit::TestCase
     @admiral = Admiral.new(@battleship)
   end
 
+  def test_the_admiral_has_a_battleship
+    assert_equal @battleship.object_id, @admiral.battleship.object_id
+  end
+
   def test_can_tell_the_battleship_to_fire
     @battleship.expect :fire!, nil
     @admiral.fire_upon_target
@@ -17,19 +21,26 @@ class TestAdmiral < MiniTest::Unit::TestCase
 end
 
 class TestBattleship< MiniTest::Unit::TestCase
+  def setup
+    @battleship = Battleship.new
+    @starting_ammunition = @battleship.ammunition
+  end
+
+  def test_starts_with_ten_rounds_of_ammunition
+     assert_equal 10, @starting_ammunition 
+  end
+
   def test_will_decrease_ammunition_when_firing
-    battleship = Battleship.new
-    starting_ammunition = battleship.ammunition
-    battleship.fire!
-    assert_equal (starting_ammunition - 1), battleship.ammunition
+    @battleship.fire!
+    assert_equal (@starting_ammunition - 1), @battleship.ammunition
   end
 end
 
-describe Battleship do
-  it "should decrease ammo" do
-    battleship = Battleship.new
-    starting_ammunition = battleship.ammunition
-    battleship.fire!
-    battleship.ammunition.must_equal (starting_ammunition -1)
-  end
-end
+#describe Battleship do
+#  it "should decrease ammo" do
+#    battleship = Battleship.new
+#    starting_ammunition = battleship.ammunition
+#    battleship.fire!
+#    battleship.ammunition.must_equal (starting_ammunition -1)
+#  end
+#end
